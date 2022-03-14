04e766245a52c3abd4cb25c065f6bb42ed01dde3
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-14 13:59:00 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -27,7 +27,7 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
+♈ YouTube : https://youtube.com/channel/UC4mrtQKsM0i31zFrd01OsRg
 ♉ Github : zeeone-ofc.github.io
 `}
 
